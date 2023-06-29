e422275078133b618079e6e57aeba9464b88bbc9
wadebooth <102829978+wadebooth@users.noreply.github.com>
2023-06-29 20:36:56 +0000

    Part One noun exercise filled out

--- main.cpp
+++ main.cpp
@@ -52,97 +52,107 @@ arm.rotateCounterClockwise();           // 4) demonstrates camelCase
 
 
 //  1)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
+//  Noun: lion
+//  action 1: the lion eats prey
+lion.eatPrey();
+//  action 2: the lion sleeps in a cave
+lion.sleepInCave();
+//  action 3: the lion feeds her children
+lion.feedChildren();
 
 //  2)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
+//  Noun: dolphin
+//  action 1: the dolphin jumps out of water
+lion.jumpOutWater();
+//  action 2: the dolphin fights a shark
+dolphin.fightShark();
+//  action 3: the dolphin becomes sentient
+dolphin.discoverReality();
 
 //  3)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
+//  Noun: Brooklyn
+//  action 1: Brooklyn snows
+brooklyn.heavySnow();
+//  action 2: Brooklyn opens a new park
+brooklyn.createPark();
+//  action 3: Brooklyn just hosted a marathon
+brooklyn.hostMarathon();
 
 //  4)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
+//  Noun: Michael Jordan
+//  action 1: Michael Jordan releases a new shoe
+mj.releaseNewShoe();
+//  action 2: MJ shoots three point attempt
+mj.shootThreePointer();
+//  action 3: MJ answers post game questions
+mj.answerReporter();
 
 //  5)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
+//  Noun: Spongebob
+//  action 1:  Spongebob makes a krabby patty
+  spongebob.cookKrabbyPatty();
+//  action 2: Spongebob feeds Gary
+  spongebob.feedGary();
+//  action 3: Spongebob tries to catch jellyfish
+  spongebob.jellyfishCatchAttempt();
 
 //  6)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
+//  Noun: maid
+//  action 1: a Maid dusts
+  maid.dust();
+//  action 2: a Maid sweeps the floor
+  maid.sweepFloor();
+//  action 3: a maid washes towels
+  maid.washTowel();
 
 //  7)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
+//  Noun: skateboard
+//  action 1: a skateboard breaks
+  skateboard.destroy();
+//  action 2: a skateboard get its wheels replaced
+  skateboard.changeWheels();
+//  action 3: a skateboard flips over
+  skateboard.flip();
 
 //  8)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
+//  Noun: EQ
+//  action 1: an EQ is bypassed
+  EQ.bypass();
+//  action 2: an EQ is automated via touch
+  EQ.touchAutomation();
+//  action 3: an EQ is muted
+  EQ.mute();
 
 //  9)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
+//  Noun: Reverb
+//  action 1: a reverb has its predelay modulated
+  reverb.modulatePreDelay();
+//  action 2: a reverb has a high pass filter added
+reverb.activateHighPassFilter();
+//  action 3: a reverb has its serial code activated for a paid subscription
+  rever.activateSubscriber();
 
 //  10)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
-
-
+//  Noun: football
+//  action 1: a football is kicked off
+football.kickoff()
+//  action 2: a football is spiked after a touchdown
+football.spike()
+//  action 3: a football has its color changed for a special event
+football.changeColor()
 
 #endif
 
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
 
- Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
- 
- If you didn't already: 
+ Commit your changes by clicking on the Source Control panel on the left,
+ entering a message, and click [Commit and push].
+
+ If you didn't already:
     Make a pull request after you make your first commit
-    pin the pull request link and this repl.it link to our DM thread in a single message.
+    pin the pull request link and this repl.it link to our DM thread in a single
+ message.
 
  send me a DM to review your pull request when the project is ready for review.
 
@@ -150,8 +160,7 @@ arm.rotateCounterClockwise();           // 4) demonstrates camelCase
  */
 
 #include <iostream>
-int main()
-{
-    std::cout << "good to go" << std::endl;
-    return 0;
+int main() {
+  std::cout << "good to go" << std::endl;
+  return 0;
 }
